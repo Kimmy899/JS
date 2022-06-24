86958aad1eb3109d8e29ebcea1f0d9c819635d66
Kimberly <105550477+Kimmy899@users.noreply.github.com>
2022-06-24 13:25:56 +0300

    Add files via upload

---
+++ 1 lesson.js
@@ -0,0 +1,94 @@
+// let yolochka4 = 4
+// let yolochka6 = 6
+// result = yolochka4 + yolochka6
+// console.log(result)
+// let yolochka_name = "zelen"
+// console.log(yolochka_name + yolochka4)
+
+
+
+// let n = 1
+// let m = 0
+// console.log(n/m)
+
+
+
+// // let t = true
+// let f = false
+// // console.log(t+f)
+// if (f) {
+//     let apples = 3+5
+//     console.log(apples)
+// } else {
+//     let meat = 2+2
+//     console.log(meat)
+// }
+
+
+
+
+// let t = true
+// let f = false
+// console.log(t+f)
+// if (f) {
+//     let apples = 3+5
+//     console.log(apples)
+// } 
+// if (t) {
+//     let milk = 2
+//     console.log(milk)
+//  } else {
+//     let meat = 2+2
+//     console.log(meat)
+//  }
+
+
+
+// let yolochka4 = 4
+// let yolochka6 = 6
+
+// let t = true
+// let f = false
+// console.log(t+f)
+
+// if (yolochka4 + yolochka6 == 11) {
+//     let apples = 3+5
+//     console.log(apples)
+// }
+// else if (yolochka4 + yolochka6 == 13) {
+//     let milk = 2
+//     console.log(milk)
+// } else {
+//     let meat = 2+2
+//     console.log(meat)
+// }
+
+
+
+
+
+let t = true
+let f = false
+// console.log(t+f)
+
+
+let age = 2
+
+let tt = typeof(age)
+console.log(tt)
+
+// if (age >= 18 && age < 60) {
+    
+//     console.log("Full content")
+// }
+// else if (age < 18){
+    
+//     console.log("Multiki")
+// }
+//     else if (age >= 60){
+    
+//         console.log("Kultura")
+// } else {
+     
+//     console.log("Good luck!")
+// }
